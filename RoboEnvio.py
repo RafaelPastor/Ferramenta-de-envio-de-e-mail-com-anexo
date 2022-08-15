78b40a6843caedddfd31da001269eafb984cd76f
RafaelPastor <rafa.f.pastor@gmail.com>
2022-08-15 07:33:18 -0300

    Iniciada a implementação do envio automatico para mais de 1 cliente

--- RoboEnvio.py
+++ RoboEnvio.py
@@ -8,62 +8,72 @@ import pandas as pd
 from email import encoders
 
 
-x = int(input('Ultima celula preenchida a ser enviada: '))
+qtdd = int(input('Ultima celula preenchida a ser enviada: '))
 
 print()
 print('Iniciado envio')
 
-#Abrindo planilha
-planilha = pd.read_excel("Envio.xlsx")  
+x = 0
+
+while x <= qtdd:
+    
+    #Abrindo planilha
+    planilha = pd.read_excel("Envio.xlsx")
 
 
-nomePessoa = planilha['NOME'] [x]
-destino = planilha['E-MAIL*'] [x]
-anexo = planilha['NOME_ANEXO+EXTENSÃO*'] [x]
+    nomePessoa = planilha['NOME'] [x]
+    destino = planilha['E-MAIL*'] [x]
+    anexo = planilha['NOME_ANEXO+EXTENSÃO*'] [x]
 
-host = "smtp.gmail.com"
-port = "587"
-login = 'Caixa postal que realizara o envio'
-senha = 'senha da caixa postal'
+    host = "smtp.gmail.com"
+    port = "587"
+    login = 'Caixa postal que realizara o envio'
+    senha = 'senha da caixa postal'
 
-server = smtplib.SMTP(host,port)
+    server = smtplib.SMTP(host,port)
 
-server.ehlo()
-server.starttls()
-server.login(login,senha)
-
-
-#E-mail
-corpo = "Corpo do E-mail"
-
-email_msg = MIMEMultipart()
-email_msg['From'] = login
-email_msg['To'] = destino
-email_msg['Subject'] = "Assunto"
-email_msg.attach(MIMEText(corpo,'html'))
-
-#Localizando anexo
-cam_arquivo = f'Anexos\\{anexo}'
-#Lendo em binário
-attachment = open(cam_arquivo, 'rb')
-#Codificando em base64
-att = MIMEBase('applicatio', 'octet-stream')
-att.set_payload(attachment.read())
-encoders.encode_base64(att)
-
-#Declarando header do anexo
-att.add_header('Content-Disposition', f'attachment; filename= {anexo}')
-attachment.close()
-
-#Anexando no e-mail
-email_msg.attach(att)
+    server.ehlo()
+    server.starttls()
+    server.login(login,senha)
 
 
-#Enviando e-mail
-server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
+    #E-mail
+    corpo = "Corpo do E-mail"
 
-#Fechando o servidor
-server.quit()
+    email_msg = MIMEMultipart()
+    email_msg['From'] = login
+    email_msg['To'] = destino
+    email_msg['Subject'] = "Assunto"
+    email_msg.attach(MIMEText(corpo,'html'))
 
-#Acompanhamento
-print(f'Finalizado envio {x + 1} para {nomePessoa} e-mail: {destino}')
+    #Localizando anexo
+    cam_arquivo = f'Anexos\\{anexo}'
+    #Lendo em binário
+    attachment = open(cam_arquivo, 'rb')
+    #Codificando em base64
+    att = MIMEBase('applicatio', 'octet-stream')
+    att.set_payload(attachment.read())
+    encoders.encode_base64(att)
+
+    #Declarando header do anexo
+    att.add_header('Content-Disposition', f'attachment; filename= {anexo}')
+    attachment.close()
+
+    #Anexando no e-mail
+    email_msg.attach(att)
+
+
+    #Enviando e-mail
+    server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
+
+    #Fechando o servidor
+    server.quit()
+
+    #Acompanhamento
+    print(f'Finalizado envio {x + 1} para {nomePessoa} e-mail: {destino}')
+
+    #Incrementação
+    x += 1
+    
+    if x >= qtdd:
+        break# Para/Finaliza o while
